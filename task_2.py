b89fc49afea54d7e693399ed9b306156c99b5615
svvvrm <svvvrm@gmail.com>
2026-09-23 20:37:16 +0300

    add comments

--- task_2.py
+++ task_2.py
@@ -4,7 +4,11 @@ c = int("1000")
 
 print("Типы:", type(a), type(b), type(c))
 print("Идентификаторы:", id(a), id(b), id(c))
+# True expected
 print("a == b:", a == b)
+# True expected
 print("a is b:", a is b)
+# True expected
 print("a == c:", a == c)
+# False expected
 print("a is c:", a is c)
